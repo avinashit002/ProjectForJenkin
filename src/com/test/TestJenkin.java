49ef0d1787045cb19bd24f3e1df03dc29c2bc88b
avinashit002 <146528222+avinashit002@users.noreply.github.com>
2024-02-03 17:24:41 +0530

    add sum operation in TestJenkin.java

--- src/com/test/TestJenkin.java
+++ src/com/test/TestJenkin.java
@@ -5,6 +5,10 @@ public class TestJenkin {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I am fine");
+		int a=2;
+		int b=3;
+		int c=a+b;
+		System.out.println("a+b= "+c);
 
 	}
 
